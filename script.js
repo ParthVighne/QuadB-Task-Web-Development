ed7e9ac6d488d2e309067407ea68ec01c100730b
Parth Vighne <89728726+ParthVighne@users.noreply.github.com>
2024-01-28 14:44:27 +0530

    Files upload

---
+++ script.js
@@ -0,0 +1,309 @@
+const newsList = [
+    {
+    "img": "./Assets/Newsitems/1.webp",
+    "title": "What Trump's return could mean for the US and the world",
+    "description": "No stopping Trump as he goes about destroying opponents, threatens donors and declares politics of vengeance against party rivals, making all simply fold"
+    },
+    {
+    "img": "./Assets/Newsitems/2.webp",
+    "title": "Bihar crisis: JD(U) hints at parting ways but Cong says 'no implosion' in INDIA bloc",
+    "description": ""
+    },
+    {
+    "img": "./Assets/Newsitems/3.webp",
+    "title": "Nitish set for another U-turn? How numbers stack up in Bihar",
+    "description": ""
+    },
+    {
+    "img": "./Assets/Newsitems/4.webp",
+    "title": "Navy warship helps vessel struck by missile in Gulf of Aden",
+    "description": "",
+    },
+    {
+    "img": "./Assets/Newsitems/5.webp",
+    "title": "Bopanna wins maiden men's doubles Slam title at Aus Open",
+    "description": "India's Rohan Bopanna and Australian Matthew Ebden dug deep to win the Australian Open men's doubles title on Saturday with a 7-6(0) 7-5 victory over Italian duo Simone Bolelli and Andrea Vavassori.",
+    },
+    {
+    "img": "./Assets/Newsitems/6.webp",
+    "title": "'Nitish likely to resign by Sunday morning': Key points",
+    "description": "",
+    },
+    {
+    "img": "./Assets/Newsitems/7.webp",
+    "title": "Why 'unhappy' Nitish Kumar may be set to go back to BJP",
+    "description": "The Bihar chief minister is said to be contemplating on taking a ‘big decision’ amid speculation about his possible return to the NDA fold ahead of 2024 polls",
+    },
+    {
+    "img": "./Assets/Newsitems/8.webp",
+    "title": "Ola's Krutrim is the first billion-dollar Indian AI startup",
+    "description": "Krutrim, India's first AI unicorn, raised $50 million in funding led by Matrix Partners. The investment valued the AI company at $1 billion. Krutrim aims to revolutionize the AI landscape globally and drive meaningful change. The funds will accelerate their mission to build India's complete AI computing stack. Krutrim recently unveiled its base large language model (LLM) with a focus on Indian languages.",
+    },
+    {
+    "img": "./Assets/Newsitems/9.webp",
+    "title": "Nasa spots Japan's lunar lander on the Moon",
+    "description": "",
+    },
+    {
+    "img": "./Assets/Newsitems/10.webp",
+    "title": "Bumrah's ability to get reverse swing leaves Root gushing",
+    "description": "Amid the focus on spinners, pacer Jasprit Bumrah on Saturday made a statement of his skills bagging two crucial wickets that helped India keep pace with England on the third day of the first Test in Hyderabad. In a dazzling display of skill and ability to extract reverse swing, Bumrah dismissed Ben Duckett and Joe Root in the middle session of the day. India's bowling coach Paras Mhambrey was stating the obvious when he rated Bumrah as an all-weather bowler. ",
+    },
+    {
+    "img": "./Assets/Newsitems/11.webp",
+    "title": "Parents of dead Indian-American student accuse police",
+    "description": "The parents of an 18-year-old Indian-American student who was found dead at the University of Illinois have filed a complaint against the school's police department for negligence. They accused the police of mishandling the search for their son, who was found dead just 400 feet away from where he was reported missing.",
+    },
+    {
+    "img": "./Assets/Newsitems/12.webp",
+    "title": "$500bn economy by F28: How Ram Mandir can fuel boom in UP",
+    "description": "",
+    },
+    {
+    "img": "./Assets/Newsitems/13.webp",
+    "title": "Trump ordered to pay victim $83m in rape defamation case",
+    "description": `Trump has repeatedly denied the allegation that he sexually assaulted Carroll in a department store dressing room in 1996, but the jury determined that in course of his rebuttal he had disparaged her, and ordered him to pay her $7.3 million for "emotional harm," $11 million for "reputational damages," and $65 million in "punitive damages."`,
+    },
+]
+
+const sidebarList = [
+    {
+        "heading": "Entertainment News",
+        "content": [{
+            "img" : "./Assets/SidebarListImages/Entertainment/1.webp",
+            "desc": "Shreyas talks to the media on his b'day post heart-attack",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Entertainment/2.webp",
+            "desc": "Bobby hugs Akshay in rare throwback PIC",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Entertainment/3.webp",
+            "desc": "Amala practices Yoga during pregnancy",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Entertainment/4.webp",
+            "desc": "Team Fighter visits a theatre, DP misses it",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Entertainment/5.webp",
+            "desc": "Raveena: 'Dil Dhadakne Do' didn't happen, but...",
+            },
+        ]
+    },
+    {
+        "heading": "Business News",
+        "content": [{
+            "img" : "./Assets/SidebarListImages/Business/1.webp",
+            "desc": "Binny Bansal exits Flipkart board",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Business/2.jpg",
+            "desc": "SpiceJet raises Rs 744 crore from investors in first tranche",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Business/3.webp",
+            "desc": "Budget 2024 income tax expectations: Will new personal tax regime be made more attractive?",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Business/4.webp",
+            "desc": "IGL eyes villages in south-west Delhi to expand PNG service",
+            },
+        ]
+    },
+    {
+        "heading": "TV News",
+        "content": [{
+            "img" : "./Assets/NewsItems/9.webp",
+            "desc": "Binny Bansal exits Flipkart board",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Entertainment/1.webp",
+            "desc": "SpiceJet raises Rs 744 crore from investors in first tranche",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Entertainment/3.webp",
+            "desc": "Budget 2024 income tax expectations: Will new personal tax regime be made more attractive?",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Business/4.webp",
+            "desc": "IGL eyes villages in south-west Delhi to expand PNG service",
+            },
+        ]
+    },
+    {
+        "heading": "Sports News",
+        "content": [{
+            "img" : "./Assets/NewsItems/5.webp",
+            "desc": "Lewis Hamilton sign flipkart deal",
+            },
+            {
+            "img" : "./Assets/NewsItems/2.webp",
+            "desc": "Winter olympics postponed due to 'Too Cold Ice'",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Business/3.webp",
+            "desc": "Sports bike sales high on rise, easy to spend money on wasteful    stuff",
+            },
+            {
+            "img" : "./Assets/SidebarListImages/Entertainment/5.webp",
+            "desc": "IGL eyes villages in south-west Delhi to expand PNG service",
+            },
+        ]
+    },
+]
+
+let inner_html = ``;
+newsList.forEach(element => {
+    inner_html += `
+    <li class="item">
+    <div class="item-image">
+        <img src=${element.img} alt="">
+    </div>
+    <div class="item-content">
+        <h3 class="item-content-title">${element.title}</h3>
+        <span class="item-content-description">${element.description}</span>
+    <div>
+    </li>
+    `;
+});
+
+let newsItems = document.querySelector(".newsItems");
+newsItems.innerHTML = inner_html;
+
+
+// LOGIC FOR SIDEBAR
+
+let sidebar_innerHTML = `
+    <div class="most-popular">
+       <h3 class="most-popular-heading">Most Popular</h3>
+       <ul class="tabstrip">
+            <li style="background: #303030;"><a href="/">SHARED</a></li>
+            <li><a href="/">COMMENTED</a></li>
+            <li><a href="/">READ</a></li>
+            <li><a href="/">TRENDING</a></li>
+       </ul>
+       <ul class="most-popular-items">
+            <li class="mp-item"><p>Amidst divorce rumours with Aishwarya Rai, Abhishek Bachchan shares a cryptic post about failures in life - See inside</p></li>
+            <li class="mp-item"><p>All players huddle in Bihar as Nitish readies to switch teams</p></li>
+            <li class="mp-item"><p>Aryabhatta National Maths Competition 2024 is now accepting registrations: Here’s how you can apply</p></li>
+            <li class="mp-item"><p>Orry reveals it was Ananya Panday who he threw out of the ‘Orry office’ because she got more famous than him: ‘Can’t have her corrupting...’</p></li>
+        </ul>
+        <span class="mp-span">SEE ALL MOST SHARED STORIES</span>
+    </div>
+`;
+sidebarList.forEach(element => {
+    
+    let listItems = ``;
+
+    element.content.forEach(item => {
+        listItems += 
+        `
+            <li class="sidebar-widget-list-item">
+                <img src=${item.img} alt="">
+                <span>${item.desc}</span>
+            </li>
+        `
+    });
+
+    sidebar_innerHTML += 
+    `
+        <div class="sidebar-widget">
+            <h3 class="sidebar-widget-title">${element.heading}</h3>
+            <ul class="sidebar-widget-list">
+                ${listItems}
+            </ul>
+        </div>
+    `;
+});
+
+let sidebar = document.querySelector(".sidebar");
+sidebar.innerHTML = sidebar_innerHTML;
+
+let footerLeft = [
+    {
+    "heading" : "Other Times Group news sites" ,
+    "items": ["The Economic Times", "Maharashtra Times", "Tamil Samayam", "I am Gujarat", "TimesPoints", "Education Times", "Mumbai Mirror", "Hindi Podcast", "Hindi Economic Times", "Vijaya Karnataka", "Malayalam Samayam", "Times Now", "India Times", "Times Food", "Bangalore Mirror", "Bengali Podcast", "Navbharat Times", "Telugu Samayam", "Ei Samay", "Times Now Navbharat", "Brand Capital", "Miss Kyra", "MX Player"]
+    },
+    {
+        "heading" : "Living and entertainment" ,
+        "items": ["iDiva", "ETimes", "Travel Destinations", "Filmfare", "Lifestyle", "Food News", "MensXP.com", "Grazia", "Bombay Times", "Online Songs", "Longwalks Age", "Times Prime", "Femina", "Zoom", "Cricbuzz.com", "TV", "Newspaper", "Whats Hot"]
+    },
+    {
+        "heading" : "Top Trends" ,
+        "items": ["iDiva", "ETimes", "Travel Destinations", "Filmfare", "Lifestyle", "Food News", "MensXP.com", "Grazia", "Bombay Times", "Online Songs", "Longwalks Age", "Times Prime", "Femina", "Zoom", "Cricbuzz.com", "TV", "Newspaper", "Whats Hot"]
+    },
+]
+
+let footerRight = [
+    {
+    "heading" : "Services" ,
+    "items": ["Ads 2 Book", "Dineout", "Times Jobs", "Gadgets Now", "ET Money Finance App", "MagicBricks", "Bolleywood News", "Careers", "CouponDunia", "Techgig", "Times Mobile", "Colombia"]
+    },
+    {
+    "heading" : "Popular Categories" ,
+    "items": ["Headlines", "India News", "World News", "Health and Fitness Tips", "Popular News", "Sports News", "Business News", "Indian TV Shows", "Celeb Photos", "Bollywood News"]
+    },
+    {
+    "heading" : "Hot on the web" ,
+    "items": ["Sania Mirza", "Salman Khan", "Arun Govil", "Panchayat 3", "Neetu Kapoor", "Anushka Sharma", "Srk", "Jawan", "Sridevi", "Shark Tank India 3"]
+    },
+    {
+    "heading" : "Trending Topics" ,
+    "items": ["Animal OTT Release", "Peyush Bansal", "Ram Mandir", "Fighter Box Office", "Jahnvi Kapoor", "Ram Lalla", "Suhana Khan",   "Baby Names", "Ananya Pandey", "Panchayat 3", "Vande Bharat Express", "Worlds Most Expensive...", "Best Laptops for students"]
+    },
+]
+
+let footerLeft_innerHTML = ``
+footerLeft.forEach(element => {
+    let footer_listItems = ``;
+    element.items.forEach(item => { 
+        footer_listItems +=
+        `
+         <li><a href="#">${item}</a></li>
+        `
+    });
+
+    footerLeft_innerHTML+=
+    `
+        <section class="footer-item">
+            <span class="footer-item-heading">${element.heading}</span>
+            <div class="footer-item-list">
+                <ul>
+                ${footer_listItems}
+                </ul>
+            </div>
+        </section>
+    `
+});
+
+let leftColumn = document.querySelector(".column-left")
+leftColumn.innerHTML = footerLeft_innerHTML;
+
+
+
+let footerRight_innerHTML = ``
+footerRight.forEach(element => {
+    let footer_listItems = ``;
+    element.items.forEach(item => { 
+        footer_listItems +=
+        `
+         <li><a href="#">${item}</a></li>
+        `
+    });
+
+    footerRight_innerHTML+=
+    `
+        <section class="footer-item">
+            <span class="footer-item-heading">${element.heading}</span>
+            <div class="footer-item-list">
+                <ul>
+                ${footer_listItems}
+                </ul>
+            </div>
+        </section>
+    `
+});
+
+let rightColumn = document.querySelector(".column-right")
+rightColumn.innerHTML = footerRight_innerHTML;
